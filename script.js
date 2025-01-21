5622dd40885acb3e3c9619cadb90ff2ca2b3f8dd
Vermeulen Ignace <ignace.vermeulen@leerling.gent.be>
2025-01-21 09:29:10 +0100

    updated js file, added main.css

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-//fetch compliments
+//fetch compliments from json file
 async function fetchCompliments(){
   try {
     const response = await fetch('./data/compliments.json');
-    if (!response.ok) throw new Error('O no, json file cannot be found');
+    if (!response.ok) throw new Error('O no, json file cannot be found'); //json file cannot be found
     const data = await response.json();
-    console.log(response, data);
+    //console.log(response, data);
+    return data.compliments;
   } catch (error) {
-      
+      console.error('Problem happened during loading:', error);
+      return ['An error has occured', 'Please try again later']; //standaard messages when error occures
   } finally {
-
+    console.log('Fetch completed');
   }
 };
 
-//main function IIFE
+//Display random compliment
+function displayRandomCompliment(compliments){
+    console.log('My compliments, Yipee', compliments);
+}
+//Main function
 (async()=>{
     const compliments = await fetchCompliments();
     const button = document.getElementById('compliment-button');
-    button.addEventListener('click', () => displayRandomCompliment(compliments))
+    button.addEventListener('click', () => displayRandomCompliment(compliments));
 })();
